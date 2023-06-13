59119ce084b273ed27775e43569f0ef360ab2683
Dorothy Kurenzvi <tkurenzvi@gmail.com>
2023-06-13 16:46:40 +0200

    Initial controller, views, models

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,16 @@
 class TicketsController < ApplicationController
+
+    def index
+     @tickets =Ticket.all
+    end
+
+    def edit 
+     @ticket = Ticket.find(params[:id])
+
+    end
+
+    def update 
+     @ticket = Ticket.find(params[:id])
+     @ticket.update(params[:ticket])
+    end
 end
