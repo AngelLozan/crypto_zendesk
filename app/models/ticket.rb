c780d07b11e090c4081119cb3366d41c48b406fd
Scott Lozano <scottloz@protonmail.com>
2023-06-13 10:41:05 +0100

    adds routes and validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,15 @@
 class Ticket < ApplicationRecord
+  before_validation :valid_email
   belongs_to :user, optional: true
   has_many :messages, through: :chatrooms
+  validates :content, presence: true
+  validates :email_address, presence: true, format: { with: /\A.*@.*\.com\z/ }
+
+  private
+
+  def valid_email
+    # TODO: return true if the email is valid, false otherwise
+    # email.match(/\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i) ? true : false
+    self.email = email.strip unless email.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tickets, only: [:index, :show, :new, :create, :edit, :update]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users do
+    resources :tickets, only: [:assign]
+  end
+
+  resources :profiles, only: [:show, :edit, :update, :new, :create]
+  
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+
 end
