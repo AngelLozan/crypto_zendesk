4cd0479970e0b153e140e3f6466f12d4102d6b98
Corentin Bulte <corentinbulte@gmail.com>
2023-06-14 17:57:41 +0200

    chatroom controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,5 @@
 class ChatroomsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
