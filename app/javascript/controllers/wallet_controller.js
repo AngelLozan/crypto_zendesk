4a31dfe8b8a81ce8d3975a2f01d78b79f57b91f0
Scott Lozano <scottloz@protonmail.com>
2023-06-14 13:50:57 +0100

    save changes

--- app/javascript/controllers/wallet_controller.js
+++ app/javascript/controllers/wallet_controller.js
@@ -3,9 +3,25 @@ import { Modal } from "bootstrap"
 
 // Connects to data-controller="wallet"
 export default class extends Controller {
+  static targets = [ "modal" ]
+
   connect() {
     console.log("Hello, Stimulus!");
   }
 
-  
+  open(e) {
+    console.log("open")
+    e.preventDefault();
+    this.modalTarget.classList.remove('d-none');
+    // document.body.classList.add('modal-open');
+  }
+
+  close(e) {
+    console.group('close')
+    e.preventDefault();
+    if (e.target !== this.modalTarget) {
+      this.modalTarget.classList.add('d-none');
+    }
+  }
+
 }
