23755069c7cdf7dfb081fd731dbc49c3402e51cb
Scott Lozano <scottloz@protonmail.com>
2023-06-14 15:51:10 +0100

    adds phantom connect

--- app/javascript/controllers/wallet_controller.js
+++ app/javascript/controllers/wallet_controller.js
@@ -1,27 +1,48 @@
-import { Controller } from "@hotwired/stimulus"
-import { Modal } from "bootstrap"
+import { Controller } from "@hotwired/stimulus";
+// import { Modal } from "bootstrap"
 
 // Connects to data-controller="wallet"
 export default class extends Controller {
-  static targets = [ "modal" ]
+  static targets = ["modal", "buttonClose", "overlay", "buttonOpen", "address"];
 
-  connect() {
+  solanaProvider;
+
+  async connect() {
     console.log("Hello, Stimulus!");
+    this.solanaProvider = this.getSolanaProvider();
+    console.log(this.solanaProvider);
   }
 
-  open(e) {
-    console.log("open")
-    e.preventDefault();
-    this.modalTarget.classList.remove('d-none');
-    // document.body.classList.add('modal-open');
+  openModal() {
+    this.modalTarget.classList.remove("hidden");
+    this.overlayTarget.classList.remove("hidden");
   }
 
-  close(e) {
-    console.group('close')
-    e.preventDefault();
-    if (e.target !== this.modalTarget) {
-      this.modalTarget.classList.add('d-none');
+  closeModal() {
+    this.modalTarget.classList.add("hidden");
+    this.overlayTarget.classList.add("hidden");
+  }
+
+  async phantomConnect() {
+    try {
+      // const resp = await this.solanaProvider.connect();
+      const resp = await window.phantom.solana.connect()
+      console.log(resp.publicKey.toString());
+      this.addressTarget.value = await resp.publicKey.toString();
+      this.closeModal();
+    } catch (err) {
+      console.log(err.message);
     }
   }
 
+  async getSolanaProvider() {
+    if ("phantom" in window) {
+      const provider = await window.phantom?.solana;
+
+      if (provider?.isPhantom) {
+        return provider;
+      }
+    }
+    window.open("https://phantom.app/", "_blank");
+  }
 }
