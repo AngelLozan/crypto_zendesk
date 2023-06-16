78c5389319006f42ba5f4d723d311d6b80e44956
Dorothy Kurenzvi <tkurenzvi@gmail.com>
2023-06-16 14:15:28 +0200

    Frontend

--- db/migrate/20230615223337_add_status_to_tickets.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToTickets < ActiveRecord::Migration[7.0]
-  def change
-    add_column :tickets, :status, :string, default: 'To Do'
-  end
-end
--- db/migrate/20230615225554_remove_status_from_tickets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromTickets < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :tickets, :status, :string
-  end
-end
