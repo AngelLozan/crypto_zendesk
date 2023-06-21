bbaa8eb665646de3fbbd25cdf318068eb9ace858
Elena Rusinova <elena.rusinova.spb@gmail.com>
2023-06-21 14:22:18 +0200

    avatar pleaceholder

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -3,10 +3,6 @@ import Swal from "sweetalert2"
 
 export default class extends Controller {
 
-  connect() {
-    console.log("hello")
-  }
-
   static values = {
     icon: String,
     alertTitle: String,
@@ -19,14 +15,23 @@ export default class extends Controller {
   initSweetalert(event) {
     event.preventDefault(); // Prevent the form to be submited after the submit button has been clicked
 
+    // fetch request to tickets create
+    // if the response has errors, add the errors to the html
+    // if it does not, display swal
+    // after click on "ok" on the swal button, redirect to chatroom show
+
     Swal.fire({
       position: 'center',
       icon: 'success',
       title: 'Your ticket has been submited! You will be redirected to a LiveChat with support',
       showConfirmButton: true,
       confirmButtonText: 'Ok',
+      inputValidator: (value) => {
+        if (!value) return 'Your text here'
+        else return null
+      }
     }).then((action) => {
       event.target[event.type](); // "submit"
     })
-  }
+}
 }
